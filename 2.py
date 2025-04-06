6707bed70ac68317b76f51f3cf1014e6dc6e1419
sunny-luoyun <2402580433@qq.com>
2025-04-06 15:50:12 +0800

    debug

--- 2.py
+++ 2.py
@@ -1,39 +1,59 @@
-''' Example of a SimNIBS tDCS leadfield in Python
-    Run with:
+"""
+Example to run TESoptimize for Temporal Interference (TI) to optimize the
+focality in the ROI vs non-ROI
 
-    simnibs_python leadfield.py
-
-    Copyright (C) 2019 Guilherme B Saturnino
-
-    place script in the main folder of the example dataset
-'''
-from simnibs import sim_struct, run_simnibs
-
-'''s = sim_struct.SESSION()
-tdcs_list = s.add_tdcslist()'''
-
-tdcs_lf = sim_struct.TDCSLEADFIELD()
-# subject folder
-tdcs_lf.subpath = 'D:\\moni\\m2m_Sub032'
-# output directory
-tdcs_lf.pathfem = 'D:\\moni\\m2m_Sub032\\leadfield'
-
-# Whether to calculate the electric field ‘E’ or current density ‘J’.
-# tdcs_lf.field = 'E'
-
-# egg_cap.csv field location
-# tdcs_lf.eeg_cap = 'D:\\app\\simNIBS\\simnibs4_examples\\m2m_ernie\\eeg_positions\\EEG10-20_extended_SPM12'
-
-# tdcs_lf.electrode = tdcs_list.add_electrode()
-
-tdcs_lf.electrode.dimensions = [15, 15]
-tdcs_lf.electrode.shape = 'rect'
-tdcs_lf.electrode.thickness = [5]
-
-# Uncoment to use the pardiso solver
-# tdcs_lf.solver_options = 'pardiso'
-# This solver is faster than the default. However, it requires much more
-# memory (~12 GB)
+Copyright (c) 2024 SimNIBS developers. Licensed under the GPL v3.
+"""
+from simnibs import opt_struct
 
 
-run_simnibs(tdcs_lf)
+''' Initialize structure '''
+opt = opt_struct.TesFlexOptimization()
+opt.subpath = '/Users/langqin/software/simnibs4_examples/m2m_ernie'                               # path of m2m folder containing the headmodel
+opt.output_folder = "/Users/langqin/software/simnibs4_examples/tes_optimize_ti_focality"
+
+''' Set up goal function '''
+opt.goal = "focality"                                   # optimize the focality of "max_TI" in the ROI ("max_TI" defined by e_postproc)
+opt.threshold = [0.1, 0.2]                              # define threshold(s) of the electric field in V/m in the non-ROI and the ROI:
+                                                        # if one threshold is defined, it is the goal that the e-field in the non-ROI is lower than this value and higher than this value in the ROI
+                                                        # if two thresholds are defined, the first one is the threshold of the non-ROI and the second one is for the ROI
+opt.e_postproc = "max_TI"                               # postprocessing of e-fields
+                                                        # "max_TI": maximal envelope of TI field magnitude
+                                                        # "dir_TI_normal": envelope of normal component
+                                                        # "dir_TI_tangential": envelope of tangential component
+''' Define first electrode pair '''
+electrode_layout = opt.add_electrode_layout("ElectrodeArrayPair")   # Pair of TES electrode arrays (here: 1 electrode per array)
+electrode_layout.radius = [10]                                      # radii of electrodes
+electrode_layout.current = [0.002, -0.002]                          # electrode currents
+
+''' Define second electrode pair '''
+electrode_layout = opt.add_electrode_layout("ElectrodeArrayPair")
+electrode_layout.radius = [10]
+electrode_layout.current = [0.002, -0.002]
+
+''' Define ROI '''
+roi = opt.add_roi()
+roi.method = "surface"
+roi.surface_type = "central"                            # define ROI on central GM surfaces
+roi.roi_sphere_center_space = "subject"
+roi.roi_sphere_center = [-41.0, -13.0,  66.0]           # center of spherical ROI in subject space (in mm)
+roi.roi_sphere_radius = 20                              # radius of spherical ROI (in mm)
+# uncomment for visual control of ROI:
+#roi.subpath = opt.subpath
+#roi.write_visualization('','roi.msh')
+
+''' Define non-ROI '''
+# all of GM surface except a spherical region with 25 mm around roi center
+non_roi = opt.add_roi()
+non_roi.method = "surface"
+non_roi.surface_type = "central"
+non_roi.roi_sphere_center_space = "subject"
+non_roi.roi_sphere_center = [-41.0, -13.0,  66.0]
+non_roi.roi_sphere_radius = 25
+non_roi.roi_sphere_operator = ["difference"]                             # take difference between GM surface and the sphere region
+# uncomment for visual control of non-ROI:
+#non_roi.subpath = opt.subpath
+#non_roi.write_visualization('','non-roi.msh')
+
+''' Run optimization '''
+opt.run()
